065e72dc47367b8f11806f922822a4b7f8d12334
naufalnashif <naufalnashif.imanuddin@Gmail.com>
2026-01-02 22:19:41 +0700

    solve gemini api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,15 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {dev} from 'node:process';
+
+// In development, load environment variables from a .env file
+if (dev) {
+  require('dotenv').config();
+}
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  })],
   model: 'googleai/gemini-2.5-flash',
 });
